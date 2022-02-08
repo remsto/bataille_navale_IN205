0611f5f95befad7d67c08e42d280966f975a3233
Rémi Gastaud <remi.gastaud@ensta-paris.fr>
2022-02-08 10:00:36 +0100

    started project and started board class

---
+++ src/main/java/ensta/model/Board.java
@@ -0,0 +1,75 @@
+package ensta.model;
+
+import ensta.model.ship.AbstractShip;
+import ensta.util.Orientation;
+
+public class Board implements IBoard {
+
+	private static final int DEFAULT_SIZE = 10;
+
+	private String name;
+	private Character[][] boats;
+	private boolean[][] hits;
+
+	public String getName() {
+		return name;
+	}
+
+	public Character[][] getBoats() {
+		return boats;
+	}
+
+	public Board() {
+	}
+
+	public Board(String name, int size) {
+		this.name = name;
+		boats = new Character[size][size];
+		hits = new boolean[size][size];
+	}
+
+	public Board(String name) {
+		this(name, 10);
+	}
+
+	public void print() {
+	}
+
+	public boolean canPutShip(AbstractShip ship, Coords coords) {
+		Orientation o = ship.getOrientation();
+		int dx = 0, dy = 0;
+		if (o == Orientation.EAST) {
+			if (coords.getX() + ship.getLength() >= this.size) {
+				return false;
+			}
+			dx = 1;
+		} else if (o == Orientation.SOUTH) {
+			if (coords.getY() + ship.getLength() >= this.size) {
+				return false;
+			}
+			dy = 1;
+		} else if (o == Orientation.NORTH) {
+			if (coords.getY() + 1 - ship.getLength() < 0) {
+				return false;
+			}
+			dy = -1;
+		} else if (o == Orientation.WEST) {
+			if (coords.getX() + 1 - ship.getLength() < 0) {
+				return false;
+			}
+			dx = -1;
+		}
+
+		Coords iCoords = new Coords(coords);
+
+		for (int i = 0; i < ship.getLength(); ++i) {
+			if (this.hasShip(iCoords)) {
+				return false;
+			}
+			iCoords.setX(iCoords.getX() + dx);
+			iCoords.setY(iCoords.getY() + dy);
+		}
+
+		return true;
+	}
+}
