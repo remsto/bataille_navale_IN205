e870323b1e77afbf90a49a36546fa2bb1e2e8441
Rémi Gastaud <remi.gastaud@ensta-paris.fr>
2022-02-08 11:31:28 +0100

    removed test files

--- test.java
+++
@@ -1,8 +0,0 @@
-import java.util.Arrays;
-
-public class test {
-    public static void main(String[] args) {
-        Character[] test_array = new Character[10];
-        System.out.println(Arrays.toString(test_array));
-    }
-}
